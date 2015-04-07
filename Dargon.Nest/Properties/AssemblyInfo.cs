9290f8cad8fb00b176c1476be85cec54b4439ff3
Michael Yu <ItzWarty@gmail.com>
2015-04-06 22:39:30 -0700

    Prepare Dargon.Nest for NuGet release.

--- Dargon.Nest/Properties/AssemblyInfo.cs
+++ Dargon.Nest/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dargon.Nest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Dargon Nest")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("The Dargon Project")]
 [assembly: AssemblyProduct("Dargon.Nest")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © The Dargon Project 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
