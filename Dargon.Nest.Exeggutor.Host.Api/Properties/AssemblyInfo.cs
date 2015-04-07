eab2e39f65e3248c56a6c3e9a887935a6f502af9
Michael Yu <ItzWarty@gmail.com>
2015-04-06 23:09:54 -0700

    Prepare Dargon.Nest.Exeggutor.Host.Api for package release.

--- Dargon.Nest.Exeggutor.Host.Api/Properties/AssemblyInfo.cs
+++ Dargon.Nest.Exeggutor.Host.Api/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dargon.Nest.Exeggutor.Host.Api")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Dargon Nest Exeggutor Service Application Host Api")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("The Dargon Project")]
 [assembly: AssemblyProduct("Dargon.Nest.Exeggutor.Host.Api")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © The Dargon Project 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
