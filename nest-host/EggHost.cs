ef6f9d65251d5bb16384e8389fd730fd55a581f4
Michael Yu <ItzWarty@gmail.com>
2015-07-14 08:41:29 -0700

    Fall back to simple-name comparison if full-name lookup returns nothing.
    This change is necessary for dargon-manager to load its ui library, and
    no harm can really be done because the simple name lookup occurs after
    the full-name lookup (alternatively, assembly lookup would just fail).

--- nest-host/EggHost.cs
+++ nest-host/EggHost.cs
@@ -26,8 +26,11 @@ namespace nest_host {
          if (!TryGetEggAssemblyPath(bootstrapArguments, out eggAssemblyPath)) {
             Console.Error.WriteLine($"Could not find nest-main.dll in \"{bootstrapArguments.EggPath}\"!");
          } else {
-            var assemblyPathsByAssemblyName = GetAssemblyPathsByAssemblyName(bootstrapArguments);
-            AppDomain.CurrentDomain.AssemblyResolve += CreateCachedAssemblyResolveHandler(assemblyPathsByAssemblyName);
+            Dictionary<string, string> assemblyPathsByAssemblySimpleName;
+            Dictionary<string, string> assemblyPathsByAssemblyFullName;
+            GetAssemblyPathsByAssemblyName(bootstrapArguments, out assemblyPathsByAssemblySimpleName, out assemblyPathsByAssemblyFullName);
+
+            AppDomain.CurrentDomain.AssemblyResolve += CreateCachedAssemblyResolveHandler(assemblyPathsByAssemblySimpleName, assemblyPathsByAssemblyFullName);
             INestApplicationEgg eggInstance = InstantiateNestApplicationEgg(eggAssemblyPath);
             var startResult = eggInstance.Start(new EggParameters(this, bootstrapArguments.Name, bootstrapArguments.PayloadBytes));
             Console.WriteLine("Egg started with " + startResult);
@@ -50,10 +53,16 @@ namespace nest_host {
          return File.Exists(eggStartPath);
       }
 
-      private ResolveEventHandler CreateCachedAssemblyResolveHandler(Dictionary<string, string> assemblyPathsByAssemblyName) {
+      private ResolveEventHandler CreateCachedAssemblyResolveHandler(Dictionary<string, string> assemblyPathsBySimpleName, Dictionary<string, string> assemblyPathsByFullName) {
          var loadedAssembliesByPath = new Dictionary<string, Assembly>();
          return (sender, e) => {
-            string assemblyPath = assemblyPathsByAssemblyName.GetValueOrDefault(e.Name);
+            Console.WriteLine("Trying to resolve: " + e.Name);
+            string assemblyPath = assemblyPathsByFullName.GetValueOrDefault(e.Name);
+            if (assemblyPath == null) {
+               var simpleName = e.Name.Substring(0, e.Name.IndexOf(','));
+               assemblyPath = assemblyPathsBySimpleName.GetValueOrDefault(simpleName);
+            }
+
             if (assemblyPath == null) {
                return null;
             } else {
@@ -67,16 +76,18 @@ namespace nest_host {
          };
       }
 
-      private Dictionary<string, string> GetAssemblyPathsByAssemblyName(BootstrapDto bootstrapArguments) {
+      private void GetAssemblyPathsByAssemblyName(BootstrapDto bootstrapArguments, out Dictionary<string, string> assemblyPathsBySimpleName, out Dictionary<string, string> assemblyPathsByFullName) {
          var loadableAssemblyPaths = Directory.GetFiles(bootstrapArguments.EggPath, "*", SearchOption.AllDirectories).Where(FilterLoadableAssemblyPaths);
-         var additionalAssemblyPathsByAssemblyName = new Dictionary<string, string>();
+         //         var additionalAssemblyPathsByAssemblyName = new Dictionary<string, string>();
+         assemblyPathsBySimpleName = new Dictionary<string, string>();
+         assemblyPathsByFullName = new Dictionary<string, string>();
          foreach (var assemblyPath in loadableAssemblyPaths) {
             try {
-               var assemblyName = AssemblyName.GetAssemblyName(assemblyPath).FullName;
-               additionalAssemblyPathsByAssemblyName.Add(assemblyName, Path.GetFullPath(assemblyPath));
+               var assemblyName = AssemblyName.GetAssemblyName(assemblyPath);
+               assemblyPathsBySimpleName.Add(assemblyName.Name, assemblyPath);
+               assemblyPathsByFullName.Add(assemblyName.FullName, assemblyPath);
             } catch (BadImageFormatException) {}
          }
-         return additionalAssemblyPathsByAssemblyName;
       }
 
       private bool FilterLoadableAssemblyPaths(string arg) {
