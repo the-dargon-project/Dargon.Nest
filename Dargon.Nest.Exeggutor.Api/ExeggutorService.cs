117a3d3a18c30effcf660eb2ea36efaa7f748f29
Michael Yu <ItzWarty@gmail.com>
2015-07-21 12:38:09 -0700

    ExeggutorService exposes KillAllHatchlingsAndUpdateAllPackages().

--- Dargon.Nest.Exeggutor.Api/ExeggutorService.cs
+++ Dargon.Nest.Exeggutor.Api/ExeggutorService.cs
@@ -6,5 +6,6 @@ namespace Dargon.Nest.Eggxecutor {
    [Guid("6ED8503F-0908-4FEC-9E05-7E507B9274C1")]
    public interface ExeggutorService {
       Guid SpawnHatchling(string eggName, SpawnConfiguration configuration);
+      void KillAllHatchlingsAndUpdateAllPackages();
    }
 }
