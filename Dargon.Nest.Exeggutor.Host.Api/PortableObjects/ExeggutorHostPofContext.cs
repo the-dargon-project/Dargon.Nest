0059f3b66c932186577dc188329490b40da91522
Michael Yu <ItzWarty@gmail.com>
2015-07-21 10:30:20 -0700

    Add ShutdownDto.

--- Dargon.Nest.Exeggutor.Host.Api/PortableObjects/ExeggutorHostPofContext.cs
+++ Dargon.Nest.Exeggutor.Host.Api/PortableObjects/ExeggutorHostPofContext.cs
@@ -4,6 +4,7 @@ namespace Dargon.Nest.Exeggutor.Host.PortableObjects {
    public class ExeggutorHostPofContext : PofContext {
       public ExeggutorHostPofContext(int basePofId) {
          RegisterPortableObjectType(basePofId + 0, typeof(BootstrapDto));
+         RegisterPortableObjectType(basePofId + 1, typeof(ShutdownDto));
       }
    }
 }
---
+++ Dargon.Nest.Exeggutor.Host.Api/PortableObjects/ShutdownDto.cs
@@ -0,0 +1,9 @@
+﻿using Dargon.PortableObjects;
+
+namespace Dargon.Nest.Exeggutor.Host.PortableObjects {
+   public class ShutdownDto : IPortableObject {
+      public ShutdownDto() { }
+      public void Serialize(IPofWriter writer) { }
+      public void Deserialize(IPofReader reader) { }
+   }
+}
