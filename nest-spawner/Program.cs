8afd7749338b512dfefd256629b9dd4f2782d490
Michael Yu <ItzWarty@gmail.com>
2015-07-22 13:51:31 -0700

    nest-spawner now has UI and code for installing Dargon.

--- nest-spawner/Program.cs
+++ nest-spawner/Program.cs
@@ -1,38 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
 using Dargon.Nest;
 using Microsoft.Win32;
 
 namespace nest_spawner {
    class Program {
+      private const string kNestSpawnerDirectoryName = "nest-spawner";
+
       public static void Main(string[] args) {
          var nestSpawnerExecutablePath = Assembly.GetEntryAssembly().Location;
+         var nestSpawnerDirectory = new FileInfo(nestSpawnerExecutablePath).Directory;
 
-         // Configure Run at Startup
-         RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-         rk.SetValue("Dargon", nestSpawnerExecutablePath);
+         if (!nestSpawnerDirectory.Name.Equals(kNestSpawnerDirectoryName, StringComparison.OrdinalIgnoreCase)) {
+            new Thread(Installer) { ApartmentState = ApartmentState.STA, IsBackground = false}.Start();
+         } else {
+            // Configure Run at Startup
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            rk.SetValue("Dargon", nestSpawnerExecutablePath);
 
-         // Update Nest Directory
-         var nestDirectory = new FileInfo(nestSpawnerExecutablePath).Directory.Parent.FullName;
-         var isInstallation = !File.Exists(Path.Combine(nestDirectory, "REMOTE"));
+            // Update Nest Directory
+            var nestDirectoryPath = nestSpawnerDirectory.Parent.FullName;
+            var isInstallation = !File.Exists(Path.Combine(nestDirectoryPath, "REMOTE"));
 
-         var nest = new LocalDargonNest(nestDirectory);
-         if (isInstallation || args.Any(s => s.Equals("--update"))) {
-            nest.Remote = "http://dargon.io/files/nest";
-            nest.Channel = "stable";
-            nest.UpdateNest();
+            var nest = new LocalDargonNest(nestDirectoryPath);
+            if (isInstallation || args.Any(s => s.Equals("--update"))) {
+               nest.Remote = "http://dargon.io/files/nest";
+               nest.Channel = "stable";
+
+               var updateNestOptions = new UpdateNestOptions();
+               var updateState = new UpdateState();
+               updateNestOptions.UpdateState = updateState;
+               Dispatcher uiThreadDispatcher = null;
+               var uiThreadReadySignal = new ManualResetEvent(false);
+               new Thread(() => {
+                  new UpdateStatusWindow() { DataContext = updateState }.Show();
+                  uiThreadDispatcher = Dispatcher.CurrentDispatcher;
+                  uiThreadReadySignal.Set();
+                  Dispatcher.Run();
+               }) { ApartmentState = ApartmentState.STA }.Start();
+               uiThreadReadySignal.WaitOne();
+               nest.UpdateNest(updateNestOptions);
+               uiThreadDispatcher.BeginInvokeShutdown(DispatcherPriority.Send);
+               uiThreadDispatcher.Thread.Join();
+            }
+
+            // Pull NestD
+            nest.ExecuteEgg("nestd", "");
+
+            // Start CoreD
+            nest.ExecuteEgg("dev-egg-runner", "-e cored -n cored");
          }
+      }
 
-         // Pull NestD
-         nest.ExecuteEgg("nestd", "");
+      private static void Installer() {
+         var dargonDirectoryDialog = new FolderBrowserDialog();
+         dargonDirectoryDialog.Description = "Where do you want to install Dargon?";
+         dargonDirectoryDialog.RootFolder = Environment.SpecialFolder.MyComputer;
+         dargonDirectoryDialog.SelectedPath = "C:/Dargon";
+         dargonDirectoryDialog.ShowNewFolderButton = true;
 
-         // Start CoreD
-         nest.ExecuteEgg("dev-egg-runner", "-e cored -n cored");
+         Console.WriteLine("Showing");
+         var dialogResult = dargonDirectoryDialog.ShowDialog();
+         if (dialogResult == DialogResult.OK) {
+            var nestPath = dargonDirectoryDialog.SelectedPath;
+            Directory.CreateDirectory(nestPath);
+            var spawnerPath = Path.Combine(nestPath, kNestSpawnerDirectoryName);
+            Directory.CreateDirectory(spawnerPath);
+
+            var entryAssembly = Assembly.GetEntryAssembly();
+            var clonedSpawnerPath = Path.Combine(spawnerPath, new FileInfo(entryAssembly.Location).Name);
+            File.Copy(entryAssembly.Location, clonedSpawnerPath);
+
+            var referencedAssemblies = entryAssembly.GetReferencedAssemblies();
+            foreach (var referencedAssembly in referencedAssemblies) {
+               var assemblyLocation = Assembly.Load(referencedAssembly.FullName).Location;
+               var excludeRegex = new Regex("microsoft|windows|\\.net", RegexOptions.IgnoreCase);
+               if (!excludeRegex.IsMatch(assemblyLocation)) {
+                  File.Copy(assemblyLocation, Path.Combine(spawnerPath, new FileInfo(assemblyLocation).Name));
+               }
+            }
+
+            Application.ApplicationExit += (s, e) => {
+               Process.Start(new ProcessStartInfo {
+                  FileName = clonedSpawnerPath,
+                  UseShellExecute = true,
+                  Arguments = "--update"
+               });
+            };
+
+            Application.Exit();
+         }
       }
    }
 }
