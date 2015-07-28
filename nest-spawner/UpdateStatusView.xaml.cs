70953389559b4b7e1f27a3cec74dfe22ed3ec680
Michael Yu <ItzWarty@gmail.com>
2015-07-28 14:03:04 -0700

    readd nest-spawner, which now has a separate solution file.

---
+++ nest-spawner/UpdateStatusView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace nest_spawner {
+   /// <summary>
+   /// Interaction logic for UpdateStatusView.xaml
+   /// </summary>
+   public partial class UpdateStatusView : UserControl {
+      public UpdateStatusView() {
+         InitializeComponent();
+      }
+   }
+}
