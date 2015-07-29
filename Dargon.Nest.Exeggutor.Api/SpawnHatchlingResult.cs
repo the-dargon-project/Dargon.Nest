e9791d541afac641a81fdc269748c3efb9d8e7dd
Michael Yu <ItzWarty@gmail.com>
2015-07-29 15:57:56 -0700

    Rename SpawnHatchlingResult.SpawnResult -> StartResult for consistency.

--- Dargon.Nest.Exeggutor.Api/SpawnHatchlingResult.cs
+++ Dargon.Nest.Exeggutor.Api/SpawnHatchlingResult.cs
@@ -5,16 +5,16 @@ using System;
 namespace Dargon.Nest.Eggxecutor {
    public class SpawnHatchlingResult : IPortableObject {
       public Guid HatchlingId { get; set; }
-      public NestResult SpawnResult { get; set; }
+      public NestResult StartResult { get; set; }
 
       public void Serialize(IPofWriter writer) {
          writer.WriteGuid(0, HatchlingId);
-         writer.WriteS32(1, (int)SpawnResult);
+         writer.WriteS32(1, (int)StartResult);
       }
 
       public void Deserialize(IPofReader reader) {
          HatchlingId = reader.ReadGuid(0);
-         SpawnResult = (NestResult)reader.ReadS32(1);
+         StartResult = (NestResult)reader.ReadS32(1);
       }
    }
 }
